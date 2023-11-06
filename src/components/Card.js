4736896c3d7f3fde8daed6c54368e965ad5e8ab0
Ankit Dahiya <71880797+ankitd2002@users.noreply.github.com>
2023-11-05 19:26:21 -0800

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import './Card.css'
 
-import './Card.css';
-
-const Card = (props) => {
-  const classes = 'card ' + props.className;
-  return <div className={classes}> {props.children}</div>;
-};
+function Card(props) {
+    return <div className='card'>
+        {props.children}
+    </div>;
+}
 
 export default Card;
